3c9cab4132adf41d2b8e5fc31a49c43f3c44def2
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-02-10 09:08:31 +0000

    GLSL switch not supported

--- data/shaders/sprite_renderer.fs
+++ data/shaders/sprite_renderer.fs
@@ -12,15 +12,21 @@ uniform sampler2D textures[8];
 void main()
 {
 	int tex_index = int(fs_tex_index);
-	switch (tex_index) {
-		case 0: pixel = texture2D(textures[0], fs_tex_coords) * fs_color; break;
-		case 1: pixel = texture2D(textures[1], fs_tex_coords) * fs_color; break;
-		case 2: pixel = texture2D(textures[2], fs_tex_coords) * fs_color; break;
-		case 3: pixel = texture2D(textures[3], fs_tex_coords) * fs_color; break;
-		case 4: pixel = texture2D(textures[4], fs_tex_coords) * fs_color; break;
-		case 5: pixel = texture2D(textures[5], fs_tex_coords) * fs_color; break;
-		case 6: pixel = texture2D(textures[6], fs_tex_coords) * fs_color; break;
-		case 7: pixel = texture2D(textures[7], fs_tex_coords) * fs_color; break;
-	}
+	if (tex_index == 0)
+		pixel = texture2D(textures[0], fs_tex_coords);
+	else if (tex_index == 1)
+		pixel = texture2D(textures[1], fs_tex_coords);
+	else if (tex_index == 2)
+		pixel = texture2D(textures[2], fs_tex_coords);
+	else if (tex_index == 3)
+		pixel = texture2D(textures[3], fs_tex_coords);
+	else if (tex_index == 4)
+		pixel = texture2D(textures[4], fs_tex_coords);
+	else if (tex_index == 5)
+		pixel = texture2D(textures[5], fs_tex_coords);
+	else if (tex_index == 6)
+		pixel = texture2D(textures[6], fs_tex_coords);
+	else if (tex_index == 7)
+		pixel = texture2D(textures[7], fs_tex_coords);
 }
 
